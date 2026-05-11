0f7306c6be945ca65d8f276bd856836cb04c1bac
peterthorpe5 <pthorpe001@dundee.ac.uk>
2026-05-11 15:05:23 +0100

    working version

---
+++ basic_runner.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+Rscript inst/scripts/00_install_dependencies.R
+
+R CMD INSTALL .
+
+Rscript inst/scripts/run_all.R \
+  --species_file=data/species.txt \
+  --override_tsv=data/species_overrides.tsv \
+  --output_dir=analysis/expression_atlas \
+  --force_download=false \
+  --force_import=false \
+  --create_duckdb=true
+
+  # To add more species later, just edit:
+
+  # data/species.txt 
+  # to add the new species and its GTEx code (if available) and then re-run the above command with --force_download=false --force_import=false to skip the download and import steps for all species and just run the analyses on the new species.
