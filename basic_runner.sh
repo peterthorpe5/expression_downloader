09a1f1c5d235e934f7aa1cf342543818e96f1a26
peterthorpe5 <pthorpe001@dundee.ac.uk>
2026-05-12 08:55:58 +0100

    download

--- basic_runner.sh
+++ basic_runner.sh
@@ -10,13 +10,21 @@ Rscript inst/scripts/00_install_dependencies.R
 
 R CMD INSTALL .
 
+
 Rscript inst/scripts/run_all.R \
   --species_file=data/species.txt \
   --override_tsv=data/species_overrides.tsv \
-  --output_dir=../analysis/expression_atlas \
+  --output_dir=../analysis/expression_atlas_v016 \
   --force_download=false \
   --force_import=false \
-  --create_duckdb=true
+  --create_duckdb=true \
+  --search_backend=arrayexpress_api \
+  --experiment_type_filter='rna|sequencing' \
+  --atlas_search_terms='RNA-seq,RNA sequencing,transcriptome,baseline' \
+  --require_expression_matrix=true \
+  --expression_file_types=tpms,fpkms
+
+
 
   # To add more species later, just edit:
 
